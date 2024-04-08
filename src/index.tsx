907eb24ab5874938786197b3b9d2f11a4aec4532
anmst21 <anmstudios21c@gmail.com>
2024-04-08 17:53:29 -0400

    mid dev commit

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import ReactDOM from "react-dom/client";
+import TextEditor from "./components/text-editor";
+
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+  return (
+    <div>
+      <TextEditor />
+    </div>
+  );
+};
+
+root.render(<App />);
